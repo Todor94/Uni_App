1d3f27fc545b3c3a70864947bb97afef6fec74c2
Todor Delev <todor.delev94@gmail.com>
2017-07-23 19:57:45 +0300

    New Version

--- src/app.js
+++ src/app.js
@@ -1,114 +1,220 @@
 const { NavigationView, Button, WebView, TextView, ui } = require('tabris');
 
-var View = new NavigationView({ left: 0, top: 0, right: 0, bottom: 0 }).appendTo(ui.contentView);
+var View = new NavigationView({ left: 0, top: 0, right: 0, bottom: 0, drawerActionVisible: true }).appendTo(ui.contentView);
+var drawer = ui.drawer;
 
-var mainPage = new tabris.Page({
-    title: 'Универсално приложение'
-}).appendTo(View);
+device.orientation = 'portrait-primary';
 
-new Button({
-    id: 'weatherBtn',
-    text: 'Времето',
-    image: {
-        src: 'Images/weather1.png',
-        scale: 3
-    }
+drawer.enabled = true;
+drawer.background = '#2DB3FF';
+drawer.on('open', () => createList());
 
-}).on('select', () => CreateWeatherSite()).appendTo(mainPage);
+function createList() {
+    new tabris.TextView({
+      left: 10, top: 10, right: 10,
+      id: 'drawerLabel',
+      text: 'Изберете опция:',
+      alignment: 'center',
+      font: "20px"
+    }).appendTo(drawer);
 
-new Button({
-    id: 'trafficBtn',
-    text: 'Трафика в София',
-    image: {
-        src: 'Images/traffic.jpeg',
-        scale: 5.4
-    }
-}).on('select', () => CreateInformationAboutTraffic()).appendTo(mainPage);
+    new Button({
+        left: 10, top: '#drawerLabel 10', right: 10,
+        id: 'mBtn',
+        text: 'Информация',
+        image: {
+            src: 'Images/tv.png',
+            scale: 55
+        }
+    }).on('select', () => createMainPage()).appendTo(drawer);
 
-new Button({
-    id: 'transportBtn',
-    text: 'Градски транспорт',
-    image: {
-        src: 'Images/metro.png',
-        scale: 1.45
-    }
-}).on('select', () => CreateInformationAboutTransport()).appendTo(mainPage);
+    new Button({
+        left: 10, top: '#mBtn 10', right: 10,
+        id: 'tBtn',
+        text: 'Транспорт и трафик',
+        image: {
+            src: 'Images/tran.jpg',
+            scale: 13
+        }
+    }).on('select', () => createTranPage()).appendTo(drawer);
 
-new Button({
-    id: 'mailBtn',
-    text: 'Поща',
-    image: {
-        src: 'Images/mail.jpg',
-        scale: 18
-    }
-}).on('select', () => CreateMailPage()).appendTo(mainPage);
+    new Button({
+        left: 10, top: '#tBtn 10', right: 10,
+        id: 'cBtn',
+        text: 'Поща, календар и бележки',
+        image: {
+            src: 'Images/check.png',
+            scale: 4
+        }
+    }).on('select', () => createCheckPage()).appendTo(drawer);
+}
 
-new Button({
-    id: 'wikiBtn',
-    text: 'Уикипедия',
-    image: {
-        src: 'Images/wikipedia.jpg',
-        scale: 4.05
-    }
-}).on('select', () => CreateWikiPage()).appendTo(mainPage);
+createMainPage();
 
-new Button({
-    id: 'navigationBtn',
-    text: 'Навигация',
-    image: {
-        src: 'Images/maps2.png',
-        scale: 5.45
-    }
-}).on('select', () => CreateNavigationPage()).appendTo(mainPage);
+function createMainPage() {
+    var mainPage = new tabris.Page({
+        title: 'Информация'
+    }).appendTo(View);
 
-new Button({
-    id: 'newsBtn',
-    text: 'Новини',
-    image: {
-        src: 'Images/News.jpg',
-        scale: 13.5
-    }
-}).on('select', () => CreateNewsPage()).appendTo(mainPage);
+    new Button({
+        id: 'weatherBtn',
+        text: 'Времето',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/weather1.png',
+            scale: 3
+        }
 
-new Button({
-    id: 'translateBtn',
-    text: 'Речник',
-    image: {
-        src: 'Images/translate.png',
-        scale: 4.15
-    }
-}).on('select', () => CreateTranslate()).appendTo(mainPage);
+    }).on('select', () => CreateWeatherSite()).appendTo(mainPage);
 
-new Button({
-    id: 'calendarBtn',
-    text: 'Календар',
-    image: {
-        src: 'Images/calendar.png',
-        scale: 2.053
-    }
-}).on('select', () => CreateCalendar()).appendTo(mainPage);
+    new Button({
+        id: 'wikiBtn',
+        text: 'Уикипедия',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/wikipedia.jpg',
+            scale: 4.05
+        }
+    }).on('select', () => CreateWikiPage()).appendTo(mainPage);
 
-new Button({
-    id: 'notesBtn',
-    text: 'Бележки',
-    image: {
-        src: 'Images/notes.png',
-        scale: 3.56
-    }
-}).on('select', () => CreateNotesPage()).appendTo(mainPage);
+    new Button({
+        id: 'newsBtn',
+        text: 'Новини',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/News.jpg',
+            scale: 13.5
+        }
+    }).on('select', () => CreateNewsPage()).appendTo(mainPage);
 
-mainPage.apply({
-    '#weatherBtn': { left: 10, right: '50%', top: '10%' },
-    '#trafficBtn': { left: 10, right: '50%', top: '30%' },
-    '#transportBtn': { left: 10, right: '50%', top: '50%' },
-    '#navigationBtn': { left: 10, right: '50%', top: '70%' },
-    '#mailBtn': { left: 10, right: '50%', top: '90%' },
-    '#newsBtn': { left: '50%', right: 10, top: '10%' },
-    '#wikiBtn': { left: '50%', right: 10, top: '30%' },
-    '#translateBtn': { left: '50%', right: 10, top: '50%' },
-    '#calendarBtn': { left: '50%', right: 10, top: '70%' },
-    '#notesBtn': { left: '50%', right: 10, top: '90%' }
-});
+    new Button({
+        id: 'translateBtn',
+        text: 'Речник',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/translate.png',
+            scale: 4.15
+        }
+    }).on('select', () => CreateTranslate()).appendTo(mainPage);
+
+    new Button({
+        id: 'progBtn',
+        text: 'TV Програма',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/tv.jpg',
+            scale: 4.05
+        }
+    }).on('select', () => CreateProggramInfo()).appendTo(mainPage);
+
+    mainPage.apply({
+        '#weatherBtn': { left: 10, right: '50%', top: '10%' },
+        '#progBtn': { left: 10, right: '50%', top: '30%' },
+        '#translateBtn': { left: 10, right: '50%', top: '50%' },
+        '#newsBtn': { left: '50%', right: 10, top: '10%' },
+        '#wikiBtn': { left: '50%', right: 10, top: '30%' },
+
+    });
+    return mainPage;
+}
+
+function createTranPage() {
+
+    var tranPage = new tabris.Page({
+        title: 'Транспорт и трафик'
+    }).appendTo(View);
+
+    new Button({
+        id: 'trafficBtn',
+        text: 'Трафик',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/traffic.jpeg',
+            scale: 5.4
+        }
+    }).on('select', () => CreateInformationAboutTraffic()).appendTo(tranPage);
+
+    new Button({
+        id: 'transportBtn',
+        text: 'Градски транспорт',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/metro.png',
+            scale: 1.45
+        }
+    }).on('select', () => CreateInformationAboutTransport()).appendTo(tranPage);
+
+    new Button({
+        id: 'navigationBtn',
+        text: 'Навигация',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/maps2.png',
+            scale: 5.45
+        }
+    }).on('select', () => CreateNavigationPage()).appendTo(tranPage);
+
+    tranPage.apply({
+        '#transportBtn': { left: 10, right: '50%', top: '10%' },
+        '#trafficBtn': { left: '50%', right: 10, top: '10%' },
+        '#navigationBtn': { left: 10, right: '50%', top: '30%' }
+    });
+    return tranPage;
+}
+
+function createCheckPage() {
+    var checkPage = new tabris.Page({
+        title: 'Поща, календар и бележки'
+    }).appendTo(View);
+
+    new Button({
+        id: 'mailBtn',
+        text: 'Поща',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/mail.jpg',
+            scale: 18
+        }
+    }).on('select', () => CreateMailPage()).appendTo(checkPage);
+
+    new Button({
+        id: 'calendarBtn',
+        text: 'Календар',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/calendar.png',
+            scale: 2.053
+        }
+    }).on('select', () => CreateCalendar()).appendTo(checkPage);
+
+    new Button({
+        id: 'notesBtn',
+        text: 'Бележки',
+        textColor: 'white',
+        background: '#2DB3FF',
+        image: {
+            src: 'Images/notes.png',
+            scale: 3.56
+        }
+    }).on('select', () => CreateNotesPage()).appendTo(checkPage);
+
+    checkPage.apply({
+        '#mailBtn': { left: 10, right: '50%', top: '10%' },
+        '#calendarBtn': { left: '50%', right: 10, top: '10%' },
+        '#notesBtn': { left: 10, right: '50%', top: '30%' }
+    });
+    return checkPage;
+}
 
 function CreateWeatherSite() {
     let WeatherPage = new tabris.Page({
@@ -160,6 +266,8 @@ function CreateMailPage() {
     new Button({
         id: 'googleMailBtn',
         text: 'Gmail',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/gmail.png',
             scale: 1.3
@@ -169,6 +277,8 @@ function CreateMailPage() {
     new Button({
         id: 'yahooMailBtn',
         text: 'Yahoo',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/yahoo.png',
             scale: 4
@@ -178,6 +288,8 @@ function CreateMailPage() {
     new Button({
         id: 'abvMailBtn',
         text: 'ABV',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/abv.png',
             scale: 4
@@ -185,7 +297,7 @@ function CreateMailPage() {
     }).on('select', () => CreateAbvMail()).appendTo(MailPage);
 
     MailPage.apply({
-        '#mailsoptions': { left: 10, right: 10, top: 50 },
+        '#mailsoptions': { left: 10, right: 10, top: '5%' },
         '#googleMailBtn': { left: 10, right: 10, top: '10%' },
         '#yahooMailBtn': { left: 10, right: 10, top: '30%' },
         '#abvMailBtn': { left: 10, right: 10, top: '50%' }
@@ -262,6 +374,8 @@ function CreateNewsPage() {
     new Button({
         id: 'newsBgBtn',
         text: 'News.bg',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/newsbg.png',
             scale: 14.3
@@ -271,6 +385,8 @@ function CreateNewsPage() {
     new Button({
         id: 'BulgariaNewsBtn',
         text: 'Bulgaria News',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/bulgariannews.png',
             scale: 0.65
@@ -280,6 +396,8 @@ function CreateNewsPage() {
     new Button({
         id: 'btvNewsBtn',
         text: 'Btv Новините',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/BTV.png',
             scale: 3.95
@@ -289,6 +407,8 @@ function CreateNewsPage() {
     new Button({
         id: 'novaNewsBtn',
         text: 'Nova News',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/nova.png',
             scale: 5.3
@@ -296,7 +416,7 @@ function CreateNewsPage() {
     }).on('select', () => CreateNovaNews()).appendTo(newsPage);
 
     newsPage.apply({
-        '#newsoptions': { left: 10, right: 10, top: 50 },
+        '#newsoptions': { left: 10, right: 10, top: '5%' },
         '#newsBgBtn': { left: 10, right: 10, top: '10%' },
         '#BulgariaNewsBtn': { left: 10, right: 10, top: '30%' },
         '#btvNewsBtn': { left: 10, right: 10, top: '50%' },
@@ -385,6 +505,8 @@ function CreateNotesPage() {
     new Button({
         id: 'gKeepBtn',
         text: 'Google Keep',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/keep.jpg',
             scale: 6.55
@@ -394,6 +516,8 @@ function CreateNotesPage() {
     new Button({
         id: 'evernoteBtn',
         text: 'Evernote',
+        textColor: 'white',
+        background: '#2DB3FF',
         image: {
             src: 'Images/evernote.png',
             scale: 20.4
@@ -401,7 +525,7 @@ function CreateNotesPage() {
     }).on('select', () => CreateENotes()).appendTo(notesPage);
 
     notesPage.apply({
-        '#notesoptions': { left: 10, right: 10, top: 50 },
+        '#notesoptions': { left: 10, right: 10, top: '5%' },
         '#gKeepBtn': { left: 10, right: 10, top: '10%' },
         '#evernoteBtn': { left: 10, right: 10, top: '30%' }
     });
@@ -427,4 +551,15 @@ function CreateENotes() {
         left: 0, right: 0, top: 0, bottom: 0,
         url: 'https://evernote.com/'
     }).appendTo(eNotesPage);
+}
+
+function CreateProggramInfo() {
+    let proggramPage = new tabris.Page({
+        title: 'Програмата'
+    }).appendTo(View);
+
+    new tabris.WebView({
+        left: 0, right: 0, top: 0, bottom: 0,
+        url: 'http://www.start.bg/lenta/tv-programa/all.html'
+    }).appendTo(proggramPage);
 }
